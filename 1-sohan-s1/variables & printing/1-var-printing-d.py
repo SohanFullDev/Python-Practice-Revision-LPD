88ccf0210d5567500f352d41d6772fabc81b4303
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-21 16:49:21 +0600

    variables & printing-d-sohan-s1

---
+++ 1-sohan-s1/variables & printing/1-var-printing-d.py
@@ -0,0 +1,13 @@
+# Example 4: getting user input
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+print("Welcome,", user_name)
+print("You are", user_age, "years old.")
+
+print('another try')
+
+user_name1 = input("enter your name: ")
+user_age1 = input("enter your age: ")
+print("Welcome,", user_name1)
+print("You are", user_age1)
