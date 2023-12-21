6fd329ec14df713fa9dda9c8102a6e5da38de9a2
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-21 16:27:37 +0600

    variables & printing-b-sohan-s1

---
+++ 1-sohan-s1/variables & printing/1-var-printing-b.py
@@ -0,0 +1,29 @@
+#  Example 2: performing mathematical operations
+x = 10
+y = 5
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+print('another try')
+
+a = 5
+b = 6
+
+jog = a+b
+biog = a-b
+gun = a*b
+vag = a/b
+
+print("Jog", jog)
+print("Biog", biog)
+print("Gun", gun)
+print("Vag", vag)
+
