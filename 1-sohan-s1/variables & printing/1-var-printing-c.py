e7ca753fe5e4ce7b7fc7c80404334268e81a2f31
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-21 16:40:19 +0600

    variables & printing-c-sohan-s1

---
+++ 1-sohan-s1/variables & printing/1-var-printing-c.py
@@ -0,0 +1,15 @@
+# Example 3: Concatenating strings
+first_name = "Sohan"
+last_name = "Sujan"
+
+full_name = first_name + " " + last_name
+greeting = "Hello, " + full_name + "!"
+
+print(greeting)
+
+print('another try')
+a_for = "apple"
+b_for = "ball"
+full_form = a_for + " " + b_for
+apple_ball = "Hey, " + full_form + "!"
+print(apple_ball)
