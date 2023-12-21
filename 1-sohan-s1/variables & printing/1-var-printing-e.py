210021d7541fdec0671e00ca3e3cdbcd6de86a7b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-21 16:53:24 +0600

    variables & printing-e-sohan-s1

---
+++ 1-sohan-s1/variables & printing/1-var-printing-e.py
@@ -0,0 +1,7 @@
+# Example 5: changing variable types
+number_string = "25"
+number_int = int(number_string)
+number_float = float(number_string)
+print("Original string:", number_string)
+print("As an integer:", number_int)
+print("As a float:", number_float)
