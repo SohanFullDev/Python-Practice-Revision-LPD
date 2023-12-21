9c9213c1cea3ebf62c24592c3e8e17507334415b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-21 16:17:23 +0600

    variables & printing-a-sohan-s1

---
+++ 1-sohan-s1/variables & printing/1-var-printing-a.py
@@ -0,0 +1,19 @@
+""" age = 30
+print(age)"""
+# multi-line comment
+
+print(310)
+
+name = 'sohan'
+print(name)
+
+# No space between print and the parenthesis
+
+
+# Example 1: Declaring variables and printing their values
+name = "Alice"
+age = 25
+height = 1.75
+print("Name:", name)  # printing a string variable
+print("Age:", age)  # printing an integer variable
+print("Height:", height)   # printing a float variable
